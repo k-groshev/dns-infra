f35ec85e918d4656fa553e046ba10f24bfc088ab
Konstantin Groshev <real.mt@gmail.com>
2022-07-08 19:34:02 +0300

    entrypoint experiment

--- hnsd/entrypoint.sh
+++ hnsd/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-set -e
+#set -e
 
 # --rs-host - recursive nameserver
 # --poll-size - size of peer pool
-exec /opt/hnsd/hnsd --pool-size 4 --rs-host 127.0.0.1:53
+#exec /opt/hnsd/hnsd --pool-size 4 --rs-host 127.0.0.1:53
+/opt/hnsd/hnsd --pool-size 4 --rs-host 0.0.0.0:53
